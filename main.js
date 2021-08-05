dcde5e462ade196f298bed205e36c8659cd76813
shash <shashwatpatel@tutanota.com>
2021-08-05 10:08:06 +0530

    added ui

--- main.js
+++ main.js
@@ -13,40 +13,82 @@ function computerPlay() {
     }
 }
 
-function playerPlay() {
-    let selection = prompt("Select Rock - Paper or Scissors");
-    return selection.toUpperCase();
-}
-
-
 function playRound(playerSelection, computerSelection) {
     
     if (playerSelection == computerSelection) {
-        return "It's a tie";
+        return 0;
 
     }else if ((playerSelection == "PAPER" && computerSelection == "ROCK") || 
-        (playerSelection == "ROCK" && computerSelection == "SCISSORS") ||
-        (playerSelection == "SCISSORS" && computerSelection == "PAPER")) {
+            (playerSelection == "ROCK" && computerSelection == "SCISSORS") ||
+            (playerSelection == "SCISSORS" && computerSelection == "PAPER")) {
 
-            return " You win!!! "  + playerSelection + " beats " + computerSelection ; 
+            return 1; 
 
-    }else if ((playerSelection == "ROCK" && computerSelection == "PAPER") || 
-        (playerSelection == "PAPER" && computerSelection == "SCISSORS") ||
-        (playerSelection == "SCISSORS" && computerSelection == "ROCK")) {
-
-        return " You loose!!! "  + computerSelection + " beats " + playerSelection ; 
-    }   
-
-}
-
-
-function game() {
-    for (let i=0; i<= 4; i++) {
-        let p1 = playerPlay();
-        let p2 = computerPlay();
-        console.log(playRound(p1, p2));
-
+    }else {
+        return -1;
     }
+
+
+} 
+
+
+const container = document.querySelector('body');
+const btn = document.querySelector('.reset');
+const p1 = document.querySelector('.p1 h1');
+const p2 = document.querySelector('.p2 h1');
+const resultAfter5 = document.querySelector('.result h1')
+
+let playerWins = 0;
+let compWins = 0;
+
+
+function resetScore(e) {
+    playerWins = 0;
+    compWins = 0;
+    console.log('hello');
 }
 
-game();
+
+function changeScore(result) {
+    if (result == 1) playerWins++;
+    else if (result == -1) compWins++;
+    p1.innerText = + playerWins;
+    p2.innerText = + compWins;
+}
+
+
+btn.addEventListener('click', () => {
+    resetScore();
+    changeScore(0);
+    btn.style.display = 'none';
+    resultAfter5.innerText = '';
+});
+
+
+const buttons = document.querySelectorAll('.button');
+buttons.forEach((button) => {
+
+    button.addEventListener('click', () => {
+      button.focus();
+      if (playerWins < 5 && compWins < 5){
+        let comp = computerPlay();
+        const result = playRound(button.id, comp);
+        changeScore(result);
+      }
+
+      if (playerWins == 5) {
+        btn.style.display = 'block';
+        resultAfter5.innerText = 'You Win !!!';
+      }
+      else if (compWins == 5) {
+        btn.style.display = 'block';
+        resultAfter5.innerText = 'You Loose !!!';
+      }
+
+    });
+  });
+
+
+
+
+
